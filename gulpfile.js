3c383041ac30742f3042e38daac1be159bda12b1
szz25 <1293239391@qq.com>
2017-12-28 21:00:37 +0800

    压缩css

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,22 @@
 var gulp = require('gulp');
-var fs = require('fs');
-var path = require('path');
 var connect = require('gulp-connect');
 var watch = require('gulp-watch');
 var webserver = require('gulp-webserver');
+var minify = require('gulp-minify-css');
+var concat = require('gulp-concat');
 gulp.task('connect', function() {
     connect.server({
         port: 8080,
         livereload: true
     })
 });
-// gulp.task('webserver', function() {
-//     gulp.src('.')
-//         .pipe(webserver({
-//             host: 'localhost',
-//             port: 8080,
-//             middleware: function(req, res, next) {
-//             }
-//         }))
-// })
+gulp.task('minify', function() {
+    gulp.src('./css/*.css')
+        .pipe(concat('ys.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('./css/'))
+})
 gulp.task('watch', function() {
     gulp.watch('index.html')
 })
-gulp.task('default', ['connect', 'watch'])
+gulp.task('default', ['connect', 'watch', 'minify'])
